d4f99ae0f0b31a60b8fdb1baaa0aabb1a5e14d63
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-10-10 18:53:58 -0400

    Orm orm orm orm

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 
 Just spent a week learning SQL basics, now its time to forget it all and us js to manage sql with SEQUELIZE.
+
+Sequelize is all about models. When using an ORM library like Sequelize, developers interact with a set of models to do database operations. Instead of interacting directly with the database, Sequelize acts as a mediator that oversees the movement of data from the database to the models and back.
+
+
--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const Sequelize = require('sequelize');
+const db = require('./db');
+const { Movie } = db.models;
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'movies.db',
-});
 
-(async () => {
+(async () => { 
+  // Sync all tables
+  await db.sequelize.sync({ force: true }) 
+
   try {
-    await sequelize.authenticate();
-    console.log('Connection to the database successful!');
+    const movie = await Movie.create({
+      title: "Debby Does Dallas",
+    });
+    console.log(movie.toJSON());
+
+    const movie2 = await Movie.create({
+      title: 'The Incredibles'
+    });
+    console.log(movie2.toJSON());
+
   } catch (error) {
     console.error('Error connecting to the database: ', error);
   }
 })();
+
+
